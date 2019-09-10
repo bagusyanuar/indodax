9d61ac1a9e192114914fbcb407c36243b0aee03b
bagusyanuar <bagus.yanuar613@gmail.com>
2019-09-10 14:00:48 +0700

    g

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel asuuuuuuuuuuu
+                    Laravel asuuuuuuuuuuu banget
                 </div>
 
                 <div class="links">
